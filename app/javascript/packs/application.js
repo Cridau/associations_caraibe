2f435c54358ed70c9fb515770d4787b513475395
Audric <audric.laroche@gmail.com>
2019-07-03 20:40:40 +0200

    update geocoding but not finished

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,6 @@
 import "bootstrap";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-import mapboxgl from 'mapbox-gl';
+import {initMapbox} from '../plugins/init_mapbox';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiY3JpZGF1IiwiYSI6ImNqeG5mYTBpYzAzMGYzY3MzazUyOG4yanEifQ.derVZ3mNEB5fsHh8-sE1Ow';
-const map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11'
-});
+initMapbox();
