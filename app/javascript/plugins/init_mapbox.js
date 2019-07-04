8a88ffc97d8036d0fef2b4ddec505da3f4b3f285
Audric <audric.laroche@gmail.com>
2019-07-04 11:06:33 +0200

    adding autocomplet and popup in map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,8 +3,11 @@ const mapElement = document.getElementById('map');
 
 const addMarkers = (map, markers) => {
   markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infowindow);
+
     new mapboxgl.Marker()
       .setLngLat([marker.lng, marker.lat])
+      .setPopup(popup)
       .addTo(map);
   });
 };
@@ -12,23 +15,25 @@ const addMarkers = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 14.5, duration: 0 });
 };
 
 
 
 const initMapbox = () => {
-  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  if(mapElement) {
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
 
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-  });
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
 
-  const markers = JSON.parse(mapElement.dataset.markers);
+    const markers = JSON.parse(mapElement.dataset.markers);
 
-  fitMapToMarkers(map, markers);
-  addMarkers(map, markers);
+    fitMapToMarkers(map, markers);
+    addMarkers(map, markers);
+  }
 };
 
 export {initMapbox};
